5d672c5d5b8599c44c0306c149c017947e17ac68
kennylam91 <phamvanlam1991@gmail.com>
2021-06-15 14:11:17 +0700

    sidebar + navbar works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Home from './pages';
 
 function App() {
   return (
-    <div className="App">
-      
-      <h1>What's upppp!</h1>
-    </div>
+    <Router>
+      <Home />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from './NavbarElements';
+import {FaBars} from 'react-icons/fa';
+
+const Navbar = ({toggle}) => {
+	return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discory">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+	)
+}
+
+export default Navbar
